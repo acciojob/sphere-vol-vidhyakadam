cc4b8d5377204a3005bc575d9b4537bd60f5483e
Vidhya Parmeshwar Kadam <131935689+vidhyakadam@users.noreply.github.com>
2023-09-21 12:43:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+    // Get the value of the Radius field from the form
+    const radius = parseFloat(document.getElementById("radius").value);
+
+    // Check if the input is a valid number
+    if (!isNaN(radius)) {
+        // Calculate the volume of the sphere
+        const volume = (4/3) * Math.PI * Math.pow(radius, 3);
+
+        // Display the calculated volume in the volume field
+        document.getElementById("volume").value = volume.toFixed(2);
+    } else {
+        // If the input is not a valid number, display an error message
+        document.getElementById("volume").value = "Invalid input";
+    }
+}
+ 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
